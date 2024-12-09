018b109009e924bd0230de8a7fa82e9174af627c
Kenzo Cogswell <kcogswell@usf.edu>
2024-12-08 20:46:03 -0500

    fixing return error in app file

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,28 @@ import BubbleChart from './components/BubbleChart'
 
 
 function App() {
-  const [chartData, setChartData] = useState(null)
-    useEffect (() => {
-      fetch('/financial_data.json')
-      .then((response) => response.json())
-      .then((data) => setChartData(data));
-    }, []);
-  
+  const [chartData, setChartData] = useState(null);
+
+  useEffect(() => {
+    fetch('public/financial_data.json')
+    .then((response) => response.json())
+    .then((data) => setChartData(data));
+  }, []);
+
   if (!chartData) {
-    return <div style={{ textAlign: 'center'}}>
+    return <div>Loading...</div>
+  }
+
+    return (
+    <div style={{ textAlign: 'center'}}>
       <h1> Dynamic Charts with React and Chart.js</h1> 
       <BarChart data ={chartData} />
       <LineChart data ={chartData} />
       <ScatterChart data ={chartData} />
       <BubbleChart data ={chartData} />
       </div>
+    )
   }
-}
+
 
 export default App;
